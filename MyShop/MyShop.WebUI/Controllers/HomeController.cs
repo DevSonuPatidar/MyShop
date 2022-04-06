ea2822677e09e2317aeff8c7adcee27b922e3e42
DevSonuPatidar <102738088+DevSonuPatidar@users.noreply.github.com>
2022-04-06 14:27:33 +0530

    Added ProductFilter but Dropdowm of filter in not styled .

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.ViewModels;
 
 namespace MyShop.WebUI.Controllers
 {
@@ -17,11 +18,25 @@ namespace MyShop.WebUI.Controllers
             context = productContext;
             productCategories = productCategoryContext;
         }
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
             List<Product> products = context.Collection().ToList();
+            List<ProductCategory> categories = productCategories.Collection().ToList(); 
 
-            return View(products);
+            if(Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
         public ActionResult Details(string Id)
         {
