ba7803ef4fb856524187d27c2d63c92b7bba3605
DevSonuPatidar <102738088+DevSonuPatidar@users.noreply.github.com>
2022-04-06 17:45:34 +0530

    Added part-2 for BasketService without creating new branch

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -15,7 +15,7 @@ namespace MyShop.Core.Models
         public string Name {get;set;}
         [StringLength(20)]
         public string Description{ get; set; }
-        public string Price{ get; set; }
+        public decimal Price{ get; set; }
         public string Category{ get; set; }
         public string Image{ get; set; }
     }
---
+++ MyShop/MyShop.Core/ViewModels/BasketSummayViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModels
+{
+    public class BasketSummayViewModel
+    {
+        public int BasketCount { get; set; }
+        public decimal BasketTotal { get; set; }
+        public BasketSummayViewModel()
+        {
+
+        }
+
+        public BasketSummayViewModel(int basketCount, decimal basketTotal)
+        {
+            this.BasketCount = basketCount;
+            this.BasketTotal = basketTotal;
+        }
+    }
+}
